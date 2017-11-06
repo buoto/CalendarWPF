56edffa024eda6d989232aa8b8bdc683f63e70a9
Michał Błotniak <buoto@buzytor.com>
2017-11-06 18:25:32 +0100

    Add project

---
+++ Calendar/RelayCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Calendar
+{
+    class RelayCommand : ICommand
+    {
+        private Action<object> action;
+        public RelayCommand(Action<object> action)
+        {
+            this.action = action;
+        }
+
+#region ICommand Members
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter != null) {
+                action(parameter);
+            } else {
+                action("NTR");
+            }
+        }
+#endregion
+    }
+}
