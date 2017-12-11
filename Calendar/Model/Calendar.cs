b2e0b5bd8aee10f6396f206786b471e78cc45153
Michał Błotniak <buoto@buzytor.com>
2017-12-12 00:49:01 +0100

    Add more weak tests

--- Calendar/Model/Calendar.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Calendar.Model
-{
-    class Calendar
-    {
-    }
-}
--- CalendarTests/ViewModel/MainWindowViewModelTests.cs
+++ CalendarTests/ViewModel/MainWindowViewModelTests.cs
@@ -1,20 +1,42 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using Calendar.Model;
+using Rhino.Mocks;
+using System.Collections.Generic;
 
 namespace Calendar.ViewModel.Tests
 {
     [TestClass()]
     public class MainWindowViewModelTests
     {
+        private Appointment appointment;
+        private IStore store;
         private MainWindowViewModel mainWindowViewModel;
+        private List<Day> daysList;
 
         [TestInitialize()]
         public void Initialize()
         {
-            //MockRepository mockRepository = new MockRepository();
-            //IStorage storage = mockRepository.StrictMock<IStorage>();
-            mainWindowViewModel = new MainWindowViewModel(new MockStore());
+            daysList = new List<Day>();
+            DateTime monday = DateTime.Now.AddDays(DayOfWeek.Monday - DateTime.Now.DayOfWeek);
+
+            for (int i = 0; i < 28; i++) {
+                daysList.Add(new Day(monday.AddDays(i)));
+            }
+            appointment = new Appointment
+            {
+                Title = "FooBar",
+                StartTime = daysList[3].DateTime,
+                EndTime = daysList[3].DateTime.AddHours(1)
+            };
+            daysList[3].Appointments.Add(appointment);
+
+            store = MockRepository.GenerateStub<IStore>();
+
+            store.Stub(s => s.GetDays(DateTime.Now)).Return(daysList);
+            store.Stub(s => s.GetDaysWithNow()).Return(daysList);
+
+            mainWindowViewModel = new MainWindowViewModel(store);
         }
 
         [TestMethod()]
@@ -31,6 +53,25 @@ namespace Calendar.ViewModel.Tests
             mainWindowViewModel.AddAppointment(mainWindowViewModel.Days[0], appointment);
 
             Assert.AreEqual("name", mainWindowViewModel.Days[0].Appointments[0].Title);
+        }
+
+        [TestMethod()]
+        public void EditAppointmentTest()
+        {
+            Appointment edited = new Appointment { Title = "name", StartTime = DateTime.Now, EndTime = DateTime.Now };
+            mainWindowViewModel.EditAppointment(appointment, edited);
+
+            Assert.AreEqual("name", mainWindowViewModel.Days[3].Appointments[0].Title);
+        }
+
+        [TestMethod()]
+        public void DeleteAppointmentTest()
+        {
+            Assert.AreEqual(1, mainWindowViewModel.Days[3].Appointments.Count);
+
+            mainWindowViewModel.DeleteAppointment(appointment);
+
+            Assert.AreEqual(0, mainWindowViewModel.Days[3].Appointments.Count);
         }
     }
 }
